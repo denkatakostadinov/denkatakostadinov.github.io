805e46bc0059222a656c6b633d855ed91cdec0b4
denkatakostadinov <40743092+denkatakostadinov@users.noreply.github.com>
2018-07-02 18:25:08 +0300

    Second commit

---
+++ index1.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html> <html lang="en" dir="ltr">
+  <head>
+<link rel="icon" type="image/png" href="logo.png" />
+ <title>Home Page</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="log-in.css" type="text/css">
+    <link rel="stylesheet" href="style.css" type="text/css">
+  </head>
+  <body class="centered-wrapper">
+    <div class="centered-content">
+<a href="login.php"><img src="logo.png" alt="Logo"></a>
+<section id="content">
+<h1><strong>
+ Home Page</strong>
+    </h1>
+			<h1>
+              Digital Signage
+            </h1>
+    </div>
+</section>
+
+  <section id="content">
+<form method="get" action="upload.php">
+    <button type="submit">User 1 <br></br>192.168.0.10</button>
+</form>
+
+<br>
+<form method="get" action="upload.php">
+    <button type="submit">User 2<br></br>192.168.0.11</button>
+
+</form>
+<br>
+<form method="get" action="">
+	<button type="submit" name="button1" id="button1"><b>+</b></button>
+</form>
+</section>
+
+  </body>
+
+  <div class="wrapper row3">
+    <footer id="footer" class="clear">
+      <p class="fl_left">Final Year Project Prototype 2018
+        <a>
+        </a>
+      </p>
+      <p class="fl_right">Version by Denis Kostadinov
+        </p>
+    </footer>
+  </div> </html>
+
